b7da7b66c61a550ee0269182fe1b21ba05d6cce9
hc09141 <hc09141@users.noreply.github.com>
2016-06-08 20:13:13 +1200

    Created editable contact fields
    Contact fields are input fields that can be edited and then that
    information is then saved to that DOM node. In the future this will be
    saved in the new Thunderbird AB database

--- react/contact.jsx
+++ react/contact.jsx
@@ -6,33 +6,45 @@ var details = {
     mobile : "Mobile"}
 
 var ContactField = React.createClass({
-    render: function() {
+  getInitialState: function() {
+        return {editing: false}
+    },
+  save: function() {
+      this.props.content = ReactDOM.findDOMNode(this.refs.newText).value;
+      this.setState({editing: false});
+  },
+  edit: function() {
+        this.setState({editing: true});
+  },
+  renderDisplay: function() {
         return (
-            <div>
-                <p>
-                    {this.props.details.name}
-                    <input type="text"/>
-                </p>
-                <p>
-                    {this.props.details.email}
-                    <input type="text"/>
-                </p>
-                <p>
-                    {this.props.details.workPhone}
-                    <input type="text"/>
-                </p>
-                <p>
-                    {this.props.details.homePhone}
-                    <input type="text"/>
-                </p>
-                <p>
-                    {this.props.details.mobile}
-                    <input type="text"/>
-                </p>
+            <div className="field">
+                <p>{this.props.fieldName} : {this.props.content}</p>
+                <span>
+                    <button onClick={this.edit}>Edit</button>
+               </span>
             </div>
-        );
+            );
+  },
+  renderForm: function() {
+        return (
+            <div className="field">
+            <p>{this.props.fieldName}</p>
+            <input type="text" ref="newText" defaultValue={this.props.content} 
+            className="form-control"></input>
+            <button onClick={this.save}>Save</button>
+            </div>
+            )
+  },
+  render: function() {
+        if (this.state.editing) {
+            return this.renderForm();
+        }
+        else {
+            return this.renderDisplay();
+        }
     }
 });
 
 ReactDOM.render(
-    <ContactField details = {details}/>, document.getElementById('contact'));
+    <ContactField fieldName="Name" content=""/>, document.getElementById('contact'));
