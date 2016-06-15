a2888c86c5b66f6f8eff74b49c802a44bb9bdfff
hc09141 <hc09141@users.noreply.github.com>
2016-06-15 22:08:24 +1200

    Introduces contact sections
    Contact sections will be used to store a grouped set of fields.

--- react/contact.jsx
+++ react/contact.jsx
@@ -1,50 +1,81 @@
-var details = {
-    name : "Name",
-    workPhone : "workPhone",
-    email : "Email",
-    homePhone : "homePhone",
-    mobile : "Mobile"}
+var Application = Components.classes["@mozilla.org/steel/application;1"].getService(Components.interfaces.steelIApplication);
+
+var details = ["Name","Work Phone", "Home Phone", "Mobile", "Email"];
 
 var ContactField = React.createClass({
-  getInitialState: function() {
+    getInitialState: function() {
         return {editing: false}
     },
-  save: function() {
-      this.props.content = ReactDOM.findDOMNode(this.refs.newText).value;
-      this.setState({editing: false});
-  },
-  edit: function() {
+    save: function() {
+        this.props.onChange(ReactDOM.findDOMNode(this.refs.newText).value, this.props.index);
+        this.setState({editing: false});
+    },
+    edit: function() {
         this.setState({editing: true});
-  },
-  renderDisplay: function() {
+    },
+    renderDisplay: function() {
         return (
             <div className="field">
-                <p>{this.props.fieldName} : {this.props.content}</p>
-                <span>
-                    <button onClick={this.edit}>Edit</button>
-               </span>
+                <p>{this.props.fieldName}
+                    : {this.props.content}</p>
+                <button onClick={this.edit}>Edit</button>
             </div>
-            );
-  },
-  renderForm: function() {
+        );
+    },
+    renderForm: function() {
         return (
             <div className="field">
-            <p>{this.props.fieldName}</p>
-            <input type="text" ref="newText" defaultValue={this.props.content} 
-            className="form-control"></input>
-            <button onClick={this.save}>Save</button>
+                <p>{this.props.fieldName}: </p>
+                <input type="text" ref="newText" defaultValue={this.props.content} className="form-control"></input>
+                <button onClick={this.save}>Save</button>
             </div>
-            )
-  },
-  render: function() {
+        )
+    },
+    render: function() {
         if (this.state.editing) {
             return this.renderForm();
-        }
-        else {
+        } else {
             return this.renderDisplay();
         }
     }
 });
 
+var ContactSection = React.createClass({
+    getInitialState: function() {
+        var fields = [];
+        for(var i = 0; i < this.props.fieldNames.length; i++){
+          fields.push({
+              id: this.nextId(),
+              fieldName: this.props.fieldNames[i],
+              content: undefined
+          });
+        }
+        return {
+          fields: fields
+        };
+    },
+    nextId: function() {
+        this.uniqueId = this.uniqueId || 0;
+        return this.uniqueId++;
+    },
+    update: function(newText, i) {
+        var fields = this.state.fields;
+        fields[i].content = newText;
+        this.setState({fields: fields});
+    },
+    eachField: function(field, i) {
+        return (
+            <ContactField key={field.id} index={i} content={field.content} fieldName = {field.fieldName} onChange={this.update}></ContactField>
+        );
+    },
+    render: function() {
+        return (
+          <div className="contact-section">
+            {this.state.fields.map(this.eachField)}
+          </div>
+        );
+    }
+});
+
 ReactDOM.render(
-    <ContactField fieldName="Name" content=""/>, document.getElementById('contact'));
+    <ContactSection fieldNames={details}/>, document.getElementById('contact'));
