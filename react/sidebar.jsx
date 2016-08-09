ce1cf229bfd6ccb354f6e527ddb147715c3eb868
joelzhuang <joely.huang@gmail.com>
2016-08-09 14:31:01 +1200

    changed srollbar to auto, and added minimal functionality to buttons, also displaying some dummy names, pair programming with Hannah and Miguel

--- react/sidebar.jsx
+++ react/sidebar.jsx
@@ -1,5 +1,46 @@
 var Application = Components.classes["@mozilla.org/steel/application;1"].getService(Components.interfaces.steelIApplication);
-
+var contacts = ["sam","Bob"];
 var ContactSidebar = React.createClass({
-
+  getInitialState: function(){
+    return{contactNames:contacts}
+  },
+  add: function(){
+    Application.console.log("added");
+  },
+  delete: function(){
+    Application.console.log("delete");
+  },
+  import: function(){
+    Application.console.log("import");
+  },
+  export: function(){
+    Application.console.log("export");
+  },
+  renderName: function(name){
+    return (
+      <p>{name}</p>
+    );
+  },
+  render: function() {
+      return (
+          <div>
+            <div id="sidebar-header">
+              <div>
+                <input id="search-bar" type="text" name ="search" placeholder="Search"></input>
+              </div>
+              <span id="buttons">
+                <input type="button" value="Export" onClick={this.export}></input>
+                <input type="button" value="Import" onClick={this.import}></input>
+                <input type="button" value="+" onClick={this.add}></input>
+              </span>
+            </div>
+            <div id="contacts-list">
+              {this.state.contactNames.map(this.renderName)}
+            </div>
+          </div>
+      );
+  },
 });
+
+ReactDOM.render(
+    <ContactSidebar contactNames={contacts}/>, document.getElementById('sidebar'));
