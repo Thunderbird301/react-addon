05d866ac2a692f78cb0cfc57044188a54625ddc9
David Barnett <david@barnett.net.nz>
2016-09-30 16:56:12 +1300

    display image from contact object

--- react/image.jsx
+++ react/image.jsx
@@ -1,13 +1,15 @@
 class ProfileImage extends React.Component {
+  handleURLRevoke() {
+    URL.revokeObjectURL(this.props.image);
+  }
   render() {
     const imageStyle = {
       borderRadius: "50%"
     };
     return (
       <div>
-        <img src={this.props.image} style={imageStyle}/>
+        <img onload={this.handleURLRevoke} src={this.props.image} style={imageStyle}/>
       </div>
-
     );
   }
 }
